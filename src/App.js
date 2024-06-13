1beb97f46294baf82f432b19a7897c377e7ffa8e
sreerag-13 <sreeragsaji41@gmail.com>
2024-06-13 21:50:18 +0530

    sample sign

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import Sign from './components/Sign';
 
 function App() {
   return (
     <div>
       <Login/>
+      <Sign/>
      
     </div>
   );
