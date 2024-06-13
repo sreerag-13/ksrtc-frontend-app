847cf5ab6a83c5d0d9243015b3ae04292d6dcee7
sreerag-13 <sreeragsaji41@gmail.com>
2024-06-13 21:35:37 +0530

    sample log

---
+++ src/components/Login.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+     
+     <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"></div>
+                <div className="row g-3">
+                                  <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Email</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label htmlFor="" className="form-label">Password</label>
+                    <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button className="btn btn-success">sigin</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Login
