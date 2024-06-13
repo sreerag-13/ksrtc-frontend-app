c014e9c2bba64d53904160094061cdc3bf25843a
sreerag-13 <sreeragsaji41@gmail.com>
2024-06-13 22:04:15 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import Sign from './components/Sign';
+import Add from './components/Add';
 
 function App() {
   return (
     <div>
       <Login/>
       <Sign/>
+      <Add/>
      
     </div>
   );
